104e4ad5edb29d92d0a14894bdbfb6d3aa25aec7
Tim Englart <timenglartdev@gmail.com>
2025-04-23 13:32:09 +1000

    format files via nix fmt

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,288 +1,288 @@
 local enable_ai = function()
-	local current_dir = vim.fn.getcwd()
-	local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
-	local code_path = home_dir .. "/code"
+  local current_dir = vim.fn.getcwd()
+  local home_dir = os.getenv('HOME') or os.getenv('USERPROFILE')
+  local code_path = home_dir .. '/code'
 
-	-- if git repo is filed under ~/code/work/private, do not allow AI
-	local private_path = code_path .. "/work/private"
-	local is_code_private = string.find(current_dir, private_path) == 1
+  -- if git repo is filed under ~/code/work/private, do not allow AI
+  local private_path = code_path .. '/work/private'
+  local is_code_private = string.find(current_dir, private_path) == 1
 
-	if is_code_private then
-		return false
-	else
-		return true
-	end
+  if is_code_private then
+    return false
+  else
+    return true
+  end
 end
 
 local prompts = {
-	-- Code related prompts
-	Explain = "Please explain how the following code works.",
-	Review = "Please review the following code and provide suggestions for improvement.",
-	Tests = "Please explain how the selected code works, then generate unit tests for it.",
-	Refactor = "Please refactor the following code to improve its clarity and readability.",
-	FixCode = "Please fix the following code to make it work as intended.",
-	FixError = "Please explain the error in the following text and provide a solution.",
-	BetterNamings = "Please provide better names for the following variables and functions.",
-	Documentation = "Please provide documentation for the following code.",
-	SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
-	SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.",
-	-- Text related prompts
-	Summarize = "Please summarize the following text.",
-	Spelling = "Please correct any grammar and spelling errors in the following text.",
-	Wording = "Please improve the grammar and wording of the following text.",
-	Concise = "Please rewrite the following text to make it more concise.",
+  -- Code related prompts
+  Explain = 'Please explain how the following code works.',
+  Review = 'Please review the following code and provide suggestions for improvement.',
+  Tests = 'Please explain how the selected code works, then generate unit tests for it.',
+  Refactor = 'Please refactor the following code to improve its clarity and readability.',
+  FixCode = 'Please fix the following code to make it work as intended.',
+  FixError = 'Please explain the error in the following text and provide a solution.',
+  BetterNamings = 'Please provide better names for the following variables and functions.',
+  Documentation = 'Please provide documentation for the following code.',
+  SwaggerApiDocs = 'Please provide documentation for the following API using Swagger.',
+  SwaggerJsDocs = 'Please write JSDoc for the following API using Swagger.',
+  -- Text related prompts
+  Summarize = 'Please summarize the following text.',
+  Spelling = 'Please correct any grammar and spelling errors in the following text.',
+  Wording = 'Please improve the grammar and wording of the following text.',
+  Concise = 'Please rewrite the following text to make it more concise.',
 }
 
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		dir = require("lazy-nix-helper").get_plugin_path("copilot.lua"),
-		cmd = "Copilot",
-		-- build = ":Copilot auth",
-		event = "InsertEnter",
-		config = function()
-			local copilot = require("copilot")
-			copilot.setup({
-				panel = {
-					enabled = false,
-					auto_refresh = true,
-				},
-				suggestion = {
-					enabled = false,
-					auto_trigger = false,
-					accept = false, -- disable built-in keymapping
-				},
-			})
+  {
+    'zbirenbaum/copilot.lua',
+    dir = require('lazy-nix-helper').get_plugin_path('copilot.lua'),
+    cmd = 'Copilot',
+    -- build = ":Copilot auth",
+    event = 'InsertEnter',
+    config = function()
+      local copilot = require('copilot')
+      copilot.setup({
+        panel = {
+          enabled = false,
+          auto_refresh = true,
+        },
+        suggestion = {
+          enabled = false,
+          auto_trigger = false,
+          accept = false, -- disable built-in keymapping
+        },
+      })
 
-			-- hide copilot suggestions when cmp menu is open
-			-- to prevent odd behavior/garbled up suggestions
-			local cmp_status_ok, cmp = pcall(require, "cmp")
-			if cmp_status_ok then
-				cmp.event:on("menu_opened", function()
-					vim.b.copilot_suggestion_hidden = true
-				end)
-				cmp.event:on("menu_closed", function()
-					vim.b.copilot_suggestion_hidden = false
-				end)
-			end
+      -- hide copilot suggestions when cmp menu is open
+      -- to prevent odd behavior/garbled up suggestions
+      local cmp_status_ok, cmp = pcall(require, 'cmp')
+      if cmp_status_ok then
+        cmp.event:on('menu_opened', function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+        cmp.event:on('menu_closed', function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
 
-			-- disable copilot if we are in a private project
-			if not enable_ai() or not copilot.setup_done then
-				vim.cmd("Copilot disable")
-				return
-			end
+      -- disable copilot if we are in a private project
+      if not enable_ai() or not copilot.setup_done then
+        vim.cmd('Copilot disable')
+        return
+      end
 
-			-- Check if we are authenticated
-			local c = require("copilot.client")
-			if c.startup_error or c.is_disabled() then
-				return
-			end
+      -- Check if we are authenticated
+      local c = require('copilot.client')
+      if c.startup_error or c.is_disabled() then
+        return
+      end
 
-			-- Disable while we do the API query
-			local client = c.get()
-			if not client then
-				return
-			end
-			vim.cmd("Copilot disable")
-			coroutine.wrap(function()
-				local api = require("copilot.api")
-				local cserr, status = api.check_status(client)
-				if not status.user or status.status == "NotAuthorized" then
-					return
-				end
-				vim.cmd("Copilot enable")
-			end)()
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		dir = require("lazy-nix-helper").get_plugin_path("which-key.nvim"),
-		optional = true,
-		opts = {
-			spec = {
-				{ "<leader>a", group = "ai" },
-				{ "<leader>gm", group = "Copilot Chat" },
-			},
-		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dir = require("lazy-nix-helper").get_plugin_path("render-markdown.nvim"),
-		optional = true,
-		opts = {
-			file_types = { "markdown", "copilot-chat" },
-		},
-		ft = { "markdown", "copilot-chat" },
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dir = require("lazy-nix-helper").get_plugin_path("CopilotChat.nvim"),
-		branch = "main",
-		-- version = "v3.3.0", -- Use a specific version to prevent breaking changes
-		dependencies = {
-			{ "nvim-telescope/telescope.nvim" }, -- Use telescope for help actions
-			{ "nvim-lua/plenary.nvim" },
-		},
-		opts = {
-			question_header = "## User ",
-			answer_header = "## Copilot ",
-			error_header = "## Error ",
-			prompts = prompts,
-			-- model = "claude-3.7-sonnet",
-			mappings = {
-				-- Use tab for completion
-				complete = {
-					detail = "Use @<Tab> or /<Tab> for options.",
-					insert = "<Tab>",
-				},
-				-- Close the chat
-				close = {
-					normal = "q",
-					insert = "<C-c>",
-				},
-				-- Reset the chat buffer
-				reset = {
-					normal = "<C-x>",
-					insert = "<C-x>",
-				},
-				-- Submit the prompt to Copilot
-				submit_prompt = {
-					normal = "<CR>",
-					insert = "<C-CR>",
-				},
-				-- Accept the diff
-				accept_diff = {
-					normal = "<C-y>",
-					insert = "<C-y>",
-				},
-				-- Show help
-				show_help = {
-					normal = "g?",
-				},
-			},
-		},
-		config = function(_, opts)
-			local chat = require("CopilotChat")
-			chat.setup(opts)
+      -- Disable while we do the API query
+      local client = c.get()
+      if not client then
+        return
+      end
+      vim.cmd('Copilot disable')
+      coroutine.wrap(function()
+        local api = require('copilot.api')
+        local _, status = api.check_status(client)
+        if not status.user or status.status == 'NotAuthorized' then
+          return
+        end
+        vim.cmd('Copilot enable')
+      end)()
+    end,
+  },
+  {
+    'folke/which-key.nvim',
+    dir = require('lazy-nix-helper').get_plugin_path('which-key.nvim'),
+    optional = true,
+    opts = {
+      spec = {
+        { '<leader>a', group = 'ai' },
+        { '<leader>gm', group = 'Copilot Chat' },
+      },
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dir = require('lazy-nix-helper').get_plugin_path('render-markdown.nvim'),
+    optional = true,
+    opts = {
+      file_types = { 'markdown', 'copilot-chat' },
+    },
+    ft = { 'markdown', 'copilot-chat' },
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dir = require('lazy-nix-helper').get_plugin_path('CopilotChat.nvim'),
+    branch = 'main',
+    -- version = "v3.3.0", -- Use a specific version to prevent breaking changes
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim' }, -- Use telescope for help actions
+      { 'nvim-lua/plenary.nvim' },
+    },
+    opts = {
+      question_header = '## User ',
+      answer_header = '## Copilot ',
+      error_header = '## Error ',
+      prompts = prompts,
+      -- model = "claude-3.7-sonnet",
+      mappings = {
+        -- Use tab for completion
+        complete = {
+          detail = 'Use @<Tab> or /<Tab> for options.',
+          insert = '<Tab>',
+        },
+        -- Close the chat
+        close = {
+          normal = 'q',
+          insert = '<C-c>',
+        },
+        -- Reset the chat buffer
+        reset = {
+          normal = '<C-x>',
+          insert = '<C-x>',
+        },
+        -- Submit the prompt to Copilot
+        submit_prompt = {
+          normal = '<CR>',
+          insert = '<C-CR>',
+        },
+        -- Accept the diff
+        accept_diff = {
+          normal = '<C-y>',
+          insert = '<C-y>',
+        },
+        -- Show help
+        show_help = {
+          normal = 'g?',
+        },
+      },
+    },
+    config = function(_, opts)
+      local chat = require('CopilotChat')
+      chat.setup(opts)
 
-			local select = require("CopilotChat.select")
-			vim.api.nvim_create_user_command("CopilotChatVisual", function(args)
-				chat.ask(args.args, { selection = select.visual })
-			end, { nargs = "*", range = true })
+      local select = require('CopilotChat.select')
+      vim.api.nvim_create_user_command('CopilotChatVisual', function(args)
+        chat.ask(args.args, { selection = select.visual })
+      end, { nargs = '*', range = true })
 
-			-- Inline chat with Copilot
-			vim.api.nvim_create_user_command("CopilotChatInline", function(args)
-				chat.ask(args.args, {
-					selection = select.visual,
-					window = {
-						layout = "float",
-						relative = "cursor",
-						width = 1,
-						height = 0.4,
-						row = 1,
-					},
-				})
-			end, { nargs = "*", range = true })
+      -- Inline chat with Copilot
+      vim.api.nvim_create_user_command('CopilotChatInline', function(args)
+        chat.ask(args.args, {
+          selection = select.visual,
+          window = {
+            layout = 'float',
+            relative = 'cursor',
+            width = 1,
+            height = 0.4,
+            row = 1,
+          },
+        })
+      end, { nargs = '*', range = true })
 
-			-- Restore CopilotChatBuffer
-			vim.api.nvim_create_user_command("CopilotChatBuffer", function(args)
-				chat.ask(args.args, { selection = select.buffer })
-			end, { nargs = "*", range = true })
+      -- Restore CopilotChatBuffer
+      vim.api.nvim_create_user_command('CopilotChatBuffer', function(args)
+        chat.ask(args.args, { selection = select.buffer })
+      end, { nargs = '*', range = true })
 
-			-- Custom buffer for CopilotChat
-			vim.api.nvim_create_autocmd("BufEnter", {
-				pattern = "copilot-*",
-				callback = function()
-					vim.opt_local.relativenumber = true
-					vim.opt_local.number = true
+      -- Custom buffer for CopilotChat
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = 'copilot-*',
+        callback = function()
+          vim.opt_local.relativenumber = true
+          vim.opt_local.number = true
 
-					-- Get current filetype and set it to markdown if the current filetype is copilot-chat
-					local ft = vim.bo.filetype
-					if ft == "copilot-chat" then
-						vim.bo.filetype = "markdown"
-					end
-				end,
-			})
-		end,
-		event = "VeryLazy",
-		keys = {
-			-- Show prompts actions with telescope
-			{
-				"<leader>ap",
-				function()
-					require("CopilotChat").select_prompt({
-						context = {
-							"buffers",
-						},
-					})
-				end,
-				desc = "CopilotChat - Prompt actions",
-			},
-			{
-				"<leader>ap",
-				function()
-					require("CopilotChat").select_prompt()
-				end,
-				mode = "x",
-				desc = "CopilotChat - Prompt actions",
-			},
-			-- Code related commands
-			{ "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-			{ "<leader>at", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-			{ "<leader>ar", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
-			{ "<leader>aR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
-			{ "<leader>an", "<cmd>CopilotChatBetterNamings<cr>", desc = "CopilotChat - Better Naming" },
-			-- Chat with Copilot in visual mode
-			{
-				"<leader>av",
-				":CopilotChatVisual",
-				mode = "x",
-				desc = "CopilotChat - Open in vertical split",
-			},
-			{
-				"<leader>ax",
-				":CopilotChatInline",
-				mode = "x",
-				desc = "CopilotChat - Inline chat",
-			},
-			-- Custom input for CopilotChat
-			{
-				"<leader>ai",
-				function()
-					local input = vim.fn.input("Ask Copilot: ")
-					if input ~= "" then
-						vim.cmd("CopilotChat " .. input)
-					end
-				end,
-				desc = "CopilotChat - Ask input",
-			},
-			-- Generate commit message based on the git diff
-			{
-				"<leader>am",
-				"<cmd>CopilotChatCommit<cr>",
-				desc = "CopilotChat - Generate commit message for all changes",
-			},
-			-- Quick chat with Copilot
-			{
-				"<leader>aq",
-				function()
-					local input = vim.fn.input("Quick Chat: ")
-					if input ~= "" then
-						vim.cmd("CopilotChatBuffer " .. input)
-					end
-				end,
-				desc = "CopilotChat - Quick chat",
-			},
-			-- Fix the issue with diagnostic
-			{ "<leader>af", "<cmd>CopilotChatFixError<cr>", desc = "CopilotChat - Fix Diagnostic" },
-			-- Clear buffer and chat history
-			{ "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
-			-- Toggle Copilot Chat Vsplit
-			{ "<leader>av", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
-			-- Copilot Chat Models
-			{ "<leader>a?", "<cmd>CopilotChatModels<cr>", desc = "CopilotChat - Select Models" },
-			-- Copilot Chat Agents
-			{ "<leader>aa", "<cmd>CopilotChatAgents<cr>", desc = "CopilotChat - Select Agents" },
-		},
-	},
+          -- Get current filetype and set it to markdown if the current filetype is copilot-chat
+          local ft = vim.bo.filetype
+          if ft == 'copilot-chat' then
+            vim.bo.filetype = 'markdown'
+          end
+        end,
+      })
+    end,
+    event = 'VeryLazy',
+    keys = {
+      -- Show prompts actions with telescope
+      {
+        '<leader>ap',
+        function()
+          require('CopilotChat').select_prompt({
+            context = {
+              'buffers',
+            },
+          })
+        end,
+        desc = 'CopilotChat - Prompt actions',
+      },
+      {
+        '<leader>ap',
+        function()
+          require('CopilotChat').select_prompt()
+        end,
+        mode = 'x',
+        desc = 'CopilotChat - Prompt actions',
+      },
+      -- Code related commands
+      { '<leader>ae', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
+      { '<leader>at', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
+      { '<leader>ar', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code' },
+      { '<leader>aR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code' },
+      { '<leader>an', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Naming' },
+      -- Chat with Copilot in visual mode
+      {
+        '<leader>av',
+        ':CopilotChatVisual',
+        mode = 'x',
+        desc = 'CopilotChat - Open in vertical split',
+      },
+      {
+        '<leader>ax',
+        ':CopilotChatInline',
+        mode = 'x',
+        desc = 'CopilotChat - Inline chat',
+      },
+      -- Custom input for CopilotChat
+      {
+        '<leader>ai',
+        function()
+          local input = vim.fn.input('Ask Copilot: ')
+          if input ~= '' then
+            vim.cmd('CopilotChat ' .. input)
+          end
+        end,
+        desc = 'CopilotChat - Ask input',
+      },
+      -- Generate commit message based on the git diff
+      {
+        '<leader>am',
+        '<cmd>CopilotChatCommit<cr>',
+        desc = 'CopilotChat - Generate commit message for all changes',
+      },
+      -- Quick chat with Copilot
+      {
+        '<leader>aq',
+        function()
+          local input = vim.fn.input('Quick Chat: ')
+          if input ~= '' then
+            vim.cmd('CopilotChatBuffer ' .. input)
+          end
+        end,
+        desc = 'CopilotChat - Quick chat',
+      },
+      -- Fix the issue with diagnostic
+      { '<leader>af', '<cmd>CopilotChatFixError<cr>', desc = 'CopilotChat - Fix Diagnostic' },
+      -- Clear buffer and chat history
+      { '<leader>al', '<cmd>CopilotChatReset<cr>', desc = 'CopilotChat - Clear buffer and chat history' },
+      -- Toggle Copilot Chat Vsplit
+      { '<leader>av', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle' },
+      -- Copilot Chat Models
+      { '<leader>a?', '<cmd>CopilotChatModels<cr>', desc = 'CopilotChat - Select Models' },
+      -- Copilot Chat Agents
+      { '<leader>aa', '<cmd>CopilotChatAgents<cr>', desc = 'CopilotChat - Select Agents' },
+    },
+  },
 }
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,122 +1,122 @@
 local has_words_before = function()
-	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-		return false
-	end
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+  if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match('^%s*$') == nil
 end
 
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dir = require("lazy-nix-helper").get_plugin_path("nvim-cmp"),
-		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
-			{ "L3MON4D3/LuaSnip", dir = require("lazy-nix-helper").get_plugin_path("luasnip") },
-			{ "saadparwaiz1/cmp_luasnip", dir = require("lazy-nix-helper").get_plugin_path("cmp_luasnip") },
+  {
+    'hrsh7th/nvim-cmp',
+    dir = require('lazy-nix-helper').get_plugin_path('nvim-cmp'),
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      { 'L3MON4D3/LuaSnip', dir = require('lazy-nix-helper').get_plugin_path('luasnip') },
+      { 'saadparwaiz1/cmp_luasnip', dir = require('lazy-nix-helper').get_plugin_path('cmp_luasnip') },
 
-			-- Adds LSP completion capabilities
-			{
-				"hrsh7th/cmp-nvim-lsp",
-				dir = require("lazy-nix-helper").get_plugin_path("cmp-nvim-lsp"),
-			},
-			{
-				"hrsh7th/cmp-nvim-lsp-signature-help",
-				dir = require("lazy-nix-helper").get_plugin_path("cmp-nvim-lsp-signature-help"),
-			},
+      -- Adds LSP completion capabilities
+      {
+        'hrsh7th/cmp-nvim-lsp',
+        dir = require('lazy-nix-helper').get_plugin_path('cmp-nvim-lsp'),
+      },
+      {
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        dir = require('lazy-nix-helper').get_plugin_path('cmp-nvim-lsp-signature-help'),
+      },
 
-			-- Adds a number of user-friendly snippets
-			{
-				"rafamadriz/friendly-snippets",
-				dir = require("lazy-nix-helper").get_plugin_path("friendly-snippets"),
-			},
-			-- LSP Kind
-			{
-				"onsails/lspkind.nvim",
-				dir = require("lazy-nix-helper").get_plugin_path("lspkind.nvim"),
-				config = function()
-					local lspkind = require("lspkind")
-					lspkind.init({
-						symbol_map = {
-							Copilot = "",
-						},
-					})
+      -- Adds a number of user-friendly snippets
+      {
+        'rafamadriz/friendly-snippets',
+        dir = require('lazy-nix-helper').get_plugin_path('friendly-snippets'),
+      },
+      -- LSP Kind
+      {
+        'onsails/lspkind.nvim',
+        dir = require('lazy-nix-helper').get_plugin_path('lspkind.nvim'),
+        config = function()
+          local lspkind = require('lspkind')
+          lspkind.init({
+            symbol_map = {
+              Copilot = '',
+            },
+          })
 
-					vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-				end,
-			},
-			-- Copilot Compare
-			{
-				"zbirenbaum/copilot-cmp",
-				dir = require("lazy-nix-helper").get_plugin_path("copilot-cmp"),
-				config = function()
-					require("copilot_cmp").setup()
-				end,
-			},
-		},
-		config = function()
-			-- [[ Configure nvim-cmp ]]
-			-- See `:help cmp`
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			require("luasnip.loaders.from_vscode").lazy_load()
-			luasnip.config.setup({})
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete({}),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<C-z>"] = cmp.mapping(function(fallback)
-						if require("copilot.suggestion").is_visible() then
-							require("copilot.suggestion").accept()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() and has_words_before() then
-							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "nvim_lsp_signature_help" },
-					{ name = "copilot", group_index = 2 },
-				},
-				formatting = {
-					format = require("lspkind").cmp_format({
-						mode = "symbol",
-						max_width = 50,
-						symbol_map = { Copilot = "" },
-					}),
-				},
-			})
-		end,
-	},
+          vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
+        end,
+      },
+      -- Copilot Compare
+      {
+        'zbirenbaum/copilot-cmp',
+        dir = require('lazy-nix-helper').get_plugin_path('copilot-cmp'),
+        config = function()
+          require('copilot_cmp').setup()
+        end,
+      },
+    },
+    config = function()
+      -- [[ Configure nvim-cmp ]]
+      -- See `:help cmp`
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      require('luasnip.loaders.from_vscode').lazy_load()
+      luasnip.config.setup({})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete({}),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ['<C-z>'] = cmp.mapping(function(fallback)
+            if require('copilot.suggestion').is_visible() then
+              require('copilot.suggestion').accept()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'nvim_lsp_signature_help' },
+          { name = 'copilot', group_index = 2 },
+        },
+        formatting = {
+          format = require('lspkind').cmp_format({
+            mode = 'symbol',
+            max_width = 50,
+            symbol_map = { Copilot = '' },
+          }),
+        },
+      })
+    end,
+  },
 }
