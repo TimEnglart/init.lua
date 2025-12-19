6e7d8cdc2a2e9d878414a875462afb7977c71eaf
Tim Englart <timenglartdev@gmail.com>
2025-12-19 15:33:58 +1000

    initialise dev-container plugin...

--- nvim/lua/plugins/devcontainer.lua
+++ nvim/lua/plugins/devcontainer.lua
@@ -3,5 +3,6 @@ return {
     'https://codeberg.org/esensar/nvim-dev-container',
     dir = require('lazy-nix-helper').get_plugin_path('nvim-dev-container'),
     dependencies = 'nvim-treesitter/nvim-treesitter',
+    opts = {},
   },
 }
